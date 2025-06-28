7f03cd34b92163dbcc2bb70d6bbbac33ca716953
Dtech2preas <lefa4082@gmail.com>
2025-06-28 05:30:33 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/minivpn/MainActivity.kt
+++ app/src/main/java/com/example/minivpn/MainActivity.kt
@@ -1,1 +1,24 @@
-h
+package com.example.minivpn
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.net.VpnService
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val connectButton = findViewById<Button>(R.id.connectButton)
+        connectButton.setOnClickListener {
+            val intent = VpnService.prepare(this)
+            if (intent != null) {
+                startActivityForResult(intent, 0)
+            } else {
+                startService(Intent(this, MinimalVpnService::class.java))
+            }
+        }
+    }
+}
