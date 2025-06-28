2582459ee9e756f0de1d84d42c13b6b9638f67bf
Dtech2preas <lefa4082@gmail.com>
2025-06-28 05:35:03 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/minivpn/MainActivity.kt
+++ app/src/main/java/com/example/minivpn/MainActivity.kt
@@ -1,23 +1,32 @@
-package com.example.minivpn
+package com.example.readyvpn
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.net.VpnService
-import android.widget.Button
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.readyvpn.databinding.ActivityMainBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val connectButton = findViewById<Button>(R.id.connectButton)
-        connectButton.setOnClickListener {
-            val intent = VpnService.prepare(this)
-            if (intent != null) {
-                startActivityForResult(intent, 0)
+        // AdMob
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
+
+        // VPN Connection
+        binding.connectButton.setOnClickListener {
+            if (VpnService.prepare(this) == null) {
+                startService(Intent(this, VpnService::class.java))
+                binding.statusText.text = "Connected"
             } else {
-                startService(Intent(this, MinimalVpnService::class.java))
+                startActivityForResult(Intent(this, VpnService::class.java), 0)
             }
         }
     }
