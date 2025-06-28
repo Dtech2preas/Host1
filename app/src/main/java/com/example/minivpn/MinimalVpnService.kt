43d503f4bda7771c93d72f78f480c98daf72eb45
Dtech2preas <lefa4082@gmail.com>
2025-06-28 05:30:51 +0200

    Update MinimalVpnService.kt

--- app/src/main/java/com/example/minivpn/MinimalVpnService.kt
+++ app/src/main/java/com/example/minivpn/MinimalVpnService.kt
@@ -1,1 +1,21 @@
-h
+package com.example.minivpn
+
+import android.net.VpnService
+import android.content.Intent
+
+class MinimalVpnService : VpnService() {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val builder = Builder()
+        builder.setSession("MinimalVPN")
+            .addAddress("10.0.0.2", 24)
+            .addDnsServer("8.8.8.8")
+            .setBlocking(true)
+
+        try {
+            builder.establish()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return START_STICKY
+    }
+}
